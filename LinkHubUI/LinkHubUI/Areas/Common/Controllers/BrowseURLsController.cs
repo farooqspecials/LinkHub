32ca602f3f263f2689b0750ac7673462add3b5f8
farooqspecials <farooqspecials@gmail.com>
2017-07-12 15:57:38 +0200

    Adding Controllers and Areas

---
+++ LinkHubUI/LinkHubUI/Areas/Common/Controllers/BrowseURLsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LinkHubUI.Areas.Common.Controllers
+{
+    public class BrowseURLsController : Controller
+    {
+        // GET: Common/BrowseURLs
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LinkHubUI/LinkHubUI/Areas/User/Controllers/URLController.cs
@@ -0,0 +1,28 @@
+﻿using BOL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LinkHubUI.Areas.User
+{
+    public class URLController : Controller
+    {
+        // GET: User/URL
+        public ActionResult Index()
+        {
+            LinkHubDbEntities db = new LinkHubDbEntities();
+            ViewBag.CategoryId = new SelectList(db.tbl_Category, "CategoryId", "CategoryName");
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            //LinkHubDbEntities db = new LinkHubDbEntities();
+            //ViewBag.CategoryId = new SelectList(db.tbl_Category, "CategoryId", "CategoryName");
+            return View();
+        }
+
+    }
+}
