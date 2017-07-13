0012835d5370f6a41b015a250fcdf866cec57812
farooqspecials <farooqspecials@gmail.com>
2017-07-13 10:35:29 +0200

    Browse URL with Approved Status

--- LinkHubUI/LinkHubUI/Areas/Common/Controllers/BrowseURLsController.cs
+++ LinkHubUI/LinkHubUI/Areas/Common/Controllers/BrowseURLsController.cs
@@ -1,17 +1,31 @@
-﻿using System;
+﻿using BLL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
+
 namespace LinkHubUI.Areas.Common.Controllers
 {
     public class BrowseURLsController : Controller
     {
+
+        private UrlBs ObjBs;
+
+        public BrowseURLsController()
+        {
+            ObjBs = new UrlBs();
+        }
+        
+
         // GET: Common/BrowseURLs
         public ActionResult Index()
         {
-            return View();
+            var Urls = ObjBs.GetALL().Where(x=>x.IsApproved=="A").ToList();
+          
+            return View(Urls);
         }
     }
 }
