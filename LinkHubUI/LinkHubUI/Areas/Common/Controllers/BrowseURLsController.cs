4870e8472e6c42d842c6d455f341ebd40a9a002b
farooqspecials <farooqspecials@gmail.com>
2017-07-13 12:25:20 +0200

    Pagination in Progress

--- LinkHubUI/LinkHubUI/Areas/Common/Controllers/BrowseURLsController.cs
+++ LinkHubUI/LinkHubUI/Areas/Common/Controllers/BrowseURLsController.cs
@@ -12,20 +12,94 @@ namespace LinkHubUI.Areas.Common.Controllers
     public class BrowseURLsController : Controller
     {
 
-        private UrlBs ObjBs;
+        private UrlBs objBs;
 
         public BrowseURLsController()
         {
-            ObjBs = new UrlBs();
+            objBs = new UrlBs();
         }
         
 
         // GET: Common/BrowseURLs
-        public ActionResult Index()
+        public ActionResult Index(string SortOrder, string SortBy)
         {
-            var Urls = ObjBs.GetALL().Where(x=>x.IsApproved=="A").ToList();
-          
-            return View(Urls);
+
+            ViewBag.SortOrder = SortOrder;
+            ViewBag.SortBy = SortBy;
+            //  var Urls = ObjBs.GetALL().Where(x=>x.IsApproved=="A").ToList();
+
+            var urls = objBs.GetALL().Where(x => x.IsApproved == "A");
+
+            switch (SortBy)
+            {
+                case "Title":
+                    switch (SortOrder)
+                    {
+
+                        case "Asc":
+                            urls = urls.OrderBy(x => x.UrlTitle).ToList();
+                            break;
+                        case "Desc":
+                            urls = urls.OrderByDescending(x => x.UrlTitle).ToList();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+
+                case "Category":
+                    switch (SortOrder)
+                    {
+
+                        case "Asc":
+                            urls = urls.OrderBy(x => x.tbl_Category.CategoryName).ToList();
+                            break;
+                        case "Desc":
+                            urls = urls.OrderByDescending(x => x.tbl_Category.CategoryName).ToList();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+
+                case "Url":
+                    switch (SortOrder)
+                    {
+
+                        case "Asc":
+                            urls = urls.OrderBy(x => x.Url).ToList();
+                            break;
+                        case "Desc":
+                            urls = urls.OrderByDescending(x => x.Url).ToList();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+
+                case "UrlDesc":
+                    switch (SortOrder)
+                    {
+
+                        case "Asc":
+                            urls = urls.OrderBy(x => x.UrlDesc).ToList();
+                            break;
+                        case "Desc":
+                            urls = urls.OrderByDescending(x => x.UrlDesc).ToList();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+
+                default:
+                    urls = urls.OrderBy(x => x.UrlTitle).ToList();
+                    break;
+            }
+
+            ViewBag.TotalPages = Math.Ceiling(objBs.GetALL().Where(x => x.IsApproved == "A").Count() / 10.0);
+
+            return View(urls);
         }
     }
 }
