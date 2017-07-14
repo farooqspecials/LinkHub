86b137c10a858ac4e7dcb9868be88a6dfc790f6f
farooqspecials <farooqspecials@gmail.com>
2017-07-14 02:57:42 +0200

    ListCat , ListUser, Delete Category

--- LinkHubUI/LinkHubUI/Areas/Admin/Controllers/ListCategoryController.cs
+++ LinkHubUI/LinkHubUI/Areas/Admin/Controllers/ListCategoryController.cs
@@ -18,12 +18,55 @@ namespace LinkHubUI.Areas.Admin
             objBs = new CategoryBs();
         }
         // GET: Admin/ListCategory
-        public ActionResult Index()
+        public ActionResult Index(string SortOrder, string SortBy, string Page)
         {
-            var cat = objBs.GetALL().ToList();
+            ViewBag.SortOrder = SortOrder;
+            ViewBag.SortBy = SortBy;
+            var cat = objBs.GetALL();
+
+            switch (SortBy)
+            {
+                case "CategoryName":
+                    switch (SortOrder)
+                    {
+
+                        case "Asc":
+                            cat = cat.OrderBy(x => x.CategoryName).ToList();
+                            break;
+                        case "Desc":
+                            cat = cat.OrderByDescending(x => x.CategoryName).ToList();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+            }
+
+            ViewBag.TotalPages = Math.Ceiling(objBs.GetALL().Count() / 10.0);
+            int page = int.Parse(Page == null ? "1" : Page);
+            ViewBag.Page = page;
+            //  urls = urls.Skip((page - 1) * 10).Take(10);
+            cat = cat.Skip((page - 1) * 10).Take(10);
             return View(cat);
 
           
+        }
+
+        public ActionResult Delete(int id)
+        {
+
+            try
+            {
+                objBs.Delete(id);
+                //objBs.categoryBs.Delete(id);
+                TempData["Msg"] = "Deleted Successfully";
+                return RedirectToAction("Index");
+            }
+            catch (Exception e1)
+            {
+                TempData["Msg"] = "Delete Failed : " + e1.Message;
+                return RedirectToAction("Index");
+            }
         }
     }
 }
